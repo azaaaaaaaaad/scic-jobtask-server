2af001c1d7541bd8a8b35b794e4fee1be536a8d2
jrazad10 <jrazad10@gmail.com>
2024-08-15 13:11:11 +0600

    mongoDB installed and implemented

--- index.js
+++ index.js
@@ -18,6 +18,31 @@ app.use(cors(corsOptions))
 app.use(express.json())
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fdffxhb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        const productsCollection = client.db('try').collection('product')
+       
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello from SoloSphere Server....')
