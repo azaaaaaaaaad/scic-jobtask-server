79e6fec7a10ce7e845b7be32dce143d7b712137d
jrazad10 <jrazad10@gmail.com>
2024-08-17 12:55:58 +0600

    updated

--- index.js
+++ index.js
@@ -37,46 +37,6 @@ async function run() {
         const products = client.db('productsDB').collection('products')
 
 
-        // app.get('/products', async (req, res) => {
-        //     const size = parseInt(req.query.size)
-        //     const page = parseInt(req.query.page) - 1
-            
-        //     const filter = req.query;
-        //     console.log(filter);
-
-        //     const query = {
-        //         ProductName: {
-        //             $regex: filter.search || '', // Default to an empty string if no search term
-        //             $options: 'i'
-        //         }
-        //     };
-
-        //     const sortOptions = {};
-
-        //     // Add sorting logic based on filter.sort
-        //     if (filter.sort === 'asc') {
-        //         sortOptions.Price = 1;
-        //     } else if (filter.sort === 'desc') {
-        //         sortOptions.Price = -1;
-        //     } else if (filter.sort === 'dateAsc') {
-        //         sortOptions.CreationDateTime = 1;
-        //     } else if (filter.sort === 'dateDesc') {
-        //         sortOptions.CreationDateTime = -1;
-        //     }
-
-        //     const options = {
-        //         sort: sortOptions
-        //     };
-
-        //     try {
-        //         const result = await products.find(query, options).skip(page*size).limit(size).toArray();
-        //         res.send(result);
-        //     } catch (err) {
-        //         console.error("Error fetching products:", err);
-        //         res.status(500).send({ error: 'Error fetching products' });
-        //     }
-        // });
-
 
         app.get('/products', async (req, res) => {
             const size = parseInt(req.query.size) || 6; // Default to 6 if not provided
